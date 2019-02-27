225a031f45546d1d1124a8741c944ebfa8e688b5
Johannes Gebhart <mgebhart56@gmail.com>
2019-02-27 20:42:32 +0100

    Version_1.1

---
+++ app/src/main/java/com/example/mgebhart16woche20/Consumer.java
@@ -0,0 +1,39 @@
+package com.example.mgebhart16woche20;
+
+public class Consumer {
+
+    public int id;
+    public String firstname;
+    public String lastname;
+
+    public Consumer(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
+
---
+++ app/src/main/java/com/example/mgebhart16woche20/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.mgebhart16woche20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    SearchView searchView;
+    String[] splitedLine;
+    List<Consumer> consumerList = new ArrayList<>();
+    List<String> consumerNames = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, consumerNames);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        searchView = findViewById(R.id.searchView);
+        readCSV();
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    public void readCSV() {
+        int id = 0;
+        String firstname = null;
+        String lastname = null;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("customers_data.csv")));
+            String line = reader.readLine();
+            while((line = reader.readLine())!= null)
+            {
+                splitedLine = line.split(",");
+
+                        id = Integer.valueOf(splitedLine[0]);
+                        firstname = splitedLine[1];
+                        lastname = splitedLine[2];
+                        Consumer c = new Consumer(id, firstname, lastname);
+                        consumerList.add(c);
+
+
+                    }
+            for(Consumer consumer : consumerList)
+            {
+                String peronName = consumer.getFirstname() + ", " + consumer.getLastname();
+                consumerNames.add(peronName);
+            }
+                //setListView(consumerList);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public void setListView(List<Consumer> list)
+    {
+
+        /*
+        for(Consumer consumer : list)
+        {
+            String firstname = consumer.getFirstname();
+            String lastname = consumer.getLastname();
+            String person = (firstname + ", " + lastname);
+            listView.setText(person);
+        }
+        */
+    }
+
+}
